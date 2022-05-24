41e5ab9bcb4924e2c2e97f55e060bfc2d9230c3a
ishwar46 <ishwarura@gmail.com>
2022-05-24 07:34:28 +0545

    GTA

---
+++ src/ClassAndObject/PracticeSet/Player.java
@@ -0,0 +1,17 @@
+package ClassAndObject.PracticeSet;
+
+public class Player {
+
+    public void hit(){
+        System.out.println("Hitting the police");
+
+    }
+
+    public void run(){
+        System.out.println("Running away from the police");
+    }
+
+    public void fire(){
+        System.out.println("Firing the gun");
+    }
+}
---
+++ src/ClassAndObject/PracticeSet/TommyVecetti.java
@@ -0,0 +1,14 @@
+package ClassAndObject.PracticeSet;
+
+public class TommyVecetti {
+
+    public static void main(String[] args) {
+
+        Player tommy = new Player();
+
+        tommy.fire();
+        tommy.hit();
+        tommy.run();
+
+    }
+}
